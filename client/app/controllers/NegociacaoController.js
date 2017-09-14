6ed4ebdb36ab97d2d8ff6059446f4d8867004b92
Maike <maike_jbe@hotmail.com>
2017-09-14 20:51:43 -0300

    Concluído o capítulo 5 - Criado um novo modelo

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,27 +1,36 @@
 class NegociacaoController
 {
     constructor() {
+
         let $ = document.querySelector.bind(document);
 
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes;
     }
 
     adiciona(event) {
+
         event.preventDefault();
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+    }
 
-        let data = DateConverter.paraData(this._inputData.value);
+    _limpaFormulario() {
 
-        let negociacao = new Negociacao(
-            data,
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-        console.log(negociacao);
-
-        let diaMesAno = DateConverter.paraTexto(negociacao.data);
-
-        console.log(diaMesAno);
     }
 }
