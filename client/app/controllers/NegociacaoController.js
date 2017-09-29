c21ae00c3b1ed9c977f54b738732b34db9d075c0
Maike <maike_jbe@hotmail.com>
2017-09-29 20:29:10 -0300

    Concluído o capítulo 12 - Trabalhando com AJAX e o padrão error first callback

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,6 +6,7 @@ class NegociacaoController
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._service = new NegociacaoService;
 
         this._negociacoes =  new Bind(
             new Negociacoes(),
@@ -48,6 +49,7 @@ class NegociacaoController
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
         this._inputData.focus();
+
     }
 
     _criaNegociacao() {
@@ -62,5 +64,21 @@ class NegociacaoController
     apaga() {
         this._negociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
+    }
+
+    importaNegociacoes() {
+
+        this._service.obterNegociacoesDaSemana(
+            (err, negociacoes) => {
+                if (err) {
+                    this._mensagem.texto = 'Não foi possível obter as negociações da semana';
+                    return;
+                }
+                negociacoes.forEach(
+                    negociacao => this._negociacoes.adiciona(negociacao)
+                );
+                this._mensagem.texto = 'Negociacoes Importadas com sucesso';
+            }
+        );
     }
 }
